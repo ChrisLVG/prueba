b4ef3a5298bca17357c3074e6e804af15b888c89
Chris <nansarel55@gmail.com>
2026-02-02 23:59:24 -0500

    Mejorando la función de cálculo de porcentajes y reorganizando la salida de resultados para mayor claridad.

--- ejercicio_cursos.py
+++ ejercicio_cursos.py
@@ -4,11 +4,9 @@ curso_minimo= 2.5
 curso_promedio_optimizado= 4
 curso_promedio_crudo= 5
 curso_maximo= 7
-total_tiempo_optimizado= curso_dalto_optimizado + curso_minimo + curso_promedio_optimizado + curso_maximo
-total_tiempo_crudo= curso_dalto_crudo + curso_minimo + curso_promedio_crudo + curso_maximo
 def calcular_porcentaje(tiempo_curso, tiempo_total):
     porcentaje = (tiempo_curso / tiempo_total) * 100
-    return int(porcentaje)
+    return round(porcentaje,2)
 
 print("Curso Dalto Optimizado: ", curso_dalto_optimizado)
 print("Curso Dalto Crudo: ", curso_dalto_crudo)
@@ -16,15 +14,14 @@ print("Curso Minimo: ", curso_minimo)
 print("Curso Promedio Optimizado: ", curso_promedio_optimizado)
 print("Curso Promedio Crudo: ", curso_promedio_crudo)
 print("Curso Maximo: ", curso_maximo)
-print("Total Tiempo Optimizado: ", total_tiempo_optimizado)
-print("Total Tiempo Crudo: ", total_tiempo_crudo)
-print("El curso de dalto optimizado representa el ", calcular_porcentaje(curso_dalto_optimizado, total_tiempo_optimizado), "% del total de tiempo optimizado")
-print("El curso minimo representa el ", calcular_porcentaje(curso_minimo, total_tiempo_optimizado), "% del total de tiempo optimizado")
-print("La diferencia en porcentaje entre el curso de dalto optimizado y el curso minimo es de: ", abs(calcular_porcentaje(curso_dalto_optimizado, total_tiempo_optimizado) - calcular_porcentaje(curso_minimo, total_tiempo_optimizado)), "%")
-print("El curso promedio representa el ", calcular_porcentaje(curso_promedio_optimizado, total_tiempo_optimizado), "% del total de tiempo optimizado")
-print("La diferencia en porcentaje entre el curso de dalto optimizado y el curso promedio es de: ", abs(calcular_porcentaje(curso_dalto_optimizado, total_tiempo_optimizado) - calcular_porcentaje(curso_promedio_optimizado, total_tiempo_optimizado)), "%")
-print("El curso maximo representa el ",calcular_porcentaje(curso_maximo, total_tiempo_optimizado), "% del total de tiempo optimizado")
-print("La diferencia en porcentaje entre el curso de dalto optimizado y el curso maximo es de: ",abs(calcular_porcentaje(curso_dalto_optimizado, total_tiempo_optimizado) - calcular_porcentaje(curso_maximo, total_tiempo_optimizado)), "%")
-
-
-
+print("----- Porcentajes -----")
+print("La diferencia en porcentaje entre el curso de dalto optimizado y el curso minimo es de: ", 100-calcular_porcentaje(curso_dalto_optimizado,curso_minimo) , "%")
+print("La diferencia en porcentaje entre el curso de dalto optimizado y el curso promedio es de: ", 100-calcular_porcentaje(curso_dalto_optimizado,curso_promedio_optimizado), "%")
+print("La diferencia en porcentaje entre el curso de dalto optimizado y el curso maximo es de: ", 100-calcular_porcentaje(curso_dalto_optimizado,curso_maximo), "%")
+print("----- Comparacion Crudo vs Optimizado -----")
+print("El curso de dalto optimizado representa una diferencia de ",round(100-calcular_porcentaje(curso_dalto_optimizado,curso_dalto_crudo), 2), "% con el curso de dalto crudo")
+print("El curso promedio optimizado representa una diferencia de ", round(100-calcular_porcentaje(curso_promedio_optimizado, curso_promedio_crudo), 2), "% con el curso promedio crudo")
+print("----- Equivalencias -----")
+print("Ver 10 horas del curso optimizado de dalto seria el equivalente a ver ", round((10 / curso_dalto_optimizado) * curso_minimo,1), " horas del curso minimo")
+print("Ver 10 horas del curso optimizado de dalto seria el equivalente a ver ", round((10 / curso_dalto_optimizado) * curso_promedio_optimizado,1), " horas del curso promedio optimizado")
+print("Ver 10 horas del curso optimizado de dalto seria el equivalente a ver ", round((10 / curso_dalto_optimizado) * curso_maximo,1), " horas del curso maximo")
