297e789fac36c3d2269455cf3693e7169a3723c4
Chris <nansarel55@gmail.com>
2026-02-11 16:41:50 -0500

    actualizando modulos

--- funciones matematicas/calculadora_palabras.py
+++
@@ -1,7 +0,0 @@
-texto= str(input("Ingrese el texto para calcular la cantidad de palabras: "))
-palabras= texto.split()#convierto el texto en una lista de palabras separadas por espacios
-print("Cantidad de palabras en el texto: ", len(palabras))
-print("tardarias aproximadamente ", len(palabras)/2, "segundos en decir esta frase")
-if len(palabras)/2 > 60:
-    print("para flaco tampoco te pedi un testamento")
-print("Dalto tardaria aproximadamente ", round(len(palabras)/2.6, 1), "segundos en decir esta frase")
