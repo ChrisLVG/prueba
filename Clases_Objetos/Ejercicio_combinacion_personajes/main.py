a6bcfd8c58464e0639a1488b15fac1c9b774686d
Chris <nansarel55@gmail.com>
2026-03-10 17:09:01 -0400

    finalizando programa

--- Clases_Objetos/Ejercicio_combinacion_personajes/main.py
+++ Clases_Objetos/Ejercicio_combinacion_personajes/main.py
@@ -1,22 +1,23 @@
 import Clases.Personaje as p    
 personajes={}
 combinaciones={}
-print("Bienvenido al juego de combinacion de personajes")
-print("Crea tu personaje combinando las habilidades de dos personajes diferentes")
-print("Selecciona una opcion")
-print("1.crear personaje")
-print("2.borrar personaje")
-print("3.combinar personajes")
-print("4.mostrar un los personajes")
-print("5.mostrar rodos personajes")
-print("6.mostrar combinacion de personajes")
-print("7.mostrar todas las combinaciones de peronajes")
-opcion= int(input("ingrese una opcion: "))
-while opcion!=7:
+while True:
+    print("Bienvenido al juego de combinacion de personajes")
+    print("Crea tu personaje combinando las habilidades de dos personajes diferentes")
+    print("Selecciona una opcion")
+    print("1.crear personaje")
+    print("2.borrar personaje")
+    print("3.combinar personajes")
+    print("4.mostrar un los personajes")
+    print("5.mostrar rodos personajes")
+    print("6.mostrar combinacion de personajes")
+    print("7.mostrar todas las combinaciones de peronajes")
+    print("8.salir")
+    opcion= int(input("ingrese una opcion: "))
     if(opcion==1):
         nombre=input("ingrese el nombre del personaje: ")
-        fuerza=input("ingrese la fuerza del personaje: ")
-        velocidad=input("ingrese la velocidad del personaje: ")
+        fuerza=int(input("ingrese la fuerza del personaje: "))
+        velocidad=int(input("ingrese la velocidad del personaje: "))
         personajes[nombre]=[fuerza,velocidad]
     elif(opcion==2):
         if(len(personajes)==0):
@@ -49,5 +50,25 @@ while opcion!=7:
         else:
             print("el personaje no existe")
     elif(opcion==5):
-        print("----- Personajes -----")
-        print(personajes)
+        if len(personajes)==0:
+            print("no hay personajes para mostrar")
+        else:
+            print("----- Personajes -----")
+            print(personajes)
+    elif(opcion==6):
+        nombre=input("ingrese el nombre de la combinacion a mostrar: ")
+        if nombre in combinaciones:
+            print(f"Combinacion: {nombre}, Fuerza: {combinaciones[nombre][0]}, Velocidad: {combinaciones[nombre][1]}")
+        else:
+            print("la combinacion no existe")
+    elif(opcion==7):
+        if len(combinaciones)==0:
+            print("no hay combinaciones para mostrar")
+        else:
+            print("----- Combinaciones -----")
+            print(combinaciones)
+    elif(opcion==8):
+        print("gracias por jugar")
+        break
+    elif(opcion<1 or opcion>8):
+        print("opcion no valida")
