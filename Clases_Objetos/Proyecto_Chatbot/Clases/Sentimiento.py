b07dc7cf7048bcaf2b8f95182b3f6c5a16f79564
Chris <nansarel55@gmail.com>
2026-03-27 21:56:46 -0400

    aclarando comentario

--- Clases_Objetos/Proyecto_Chatbot/Clases/Sentimiento.py
+++ Clases_Objetos/Proyecto_Chatbot/Clases/Sentimiento.py
@@ -4,5 +4,5 @@ class Sentimiento:
         self.color = color
 
     def __str__(self):
-        # Pinta solo el texto del sentimiento y vuelve al color normal inmediatamente
+        # Pinta solo el texto del sentimiento y vuelve al color normal inmediatamente reemplazando los: {}, por los parametros color y texto en ese orden, el \x1b[0m es para volver al color normal despues de pintar el texto del sentimiento
         return "\x1b[1;{}m{}\x1b[0m".format(self.color, self.texto)
